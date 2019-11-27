9735a91e49dfda557ef3faf4f993c63c8d08e1f1
Nicklas Jarnesjö <nicklas@webready.se>
2019-11-27 09:15:47 +0100

    Bump version

--- dist/js/imcrop.worker.detect.js
+++ dist/js/imcrop.worker.detect.js
@@ -1,7 +1,7 @@
 /**
  * @infomaker/cropjs - Image soft cropper for predefined crop definitions featuring automatic crop suggestions. Created by Infomaker Scandinavia AB
  * @author Danne Lundqvist <danne.lundqvist@infomaker.se>
- * @version v2.0.0
+ * @version v2.1.0
  * @link http://www.infomaker.se
  * @license MIT
  */
